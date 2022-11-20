8abb765a5423eb0adf532477b09ae7a1897cc997
KonnorZzz <xzhang21@inside.artcenter.edu>
2022-11-20 14:56:22 -0800

    OnContactChangeColor

---
+++ Assets/ConnorScripts/ChangingHands.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingHands : MonoBehaviour
+{
+    public GameObject grassHandLeft;
+    public GameObject fireHandLeft;
+    public GameObject waterHandLeft;
+
+    public GameObject grassHandRight;
+    public GameObject fireHandRight;
+    public GameObject waterHandRight; 
+
+    void Start()
+    {
+        grassHandLeft.SetActive(false);
+        fireHandLeft.SetActive(false); 
+        waterHandLeft.SetActive(false);
+
+        grassHandRight.SetActive(false); 
+        waterHandRight.SetActive (false);
+        fireHandRight.SetActive(false); 
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void OnGrassLeft()
+    {
+        fireHandLeft.SetActive(false);
+        waterHandLeft.SetActive(false);
+        grassHandLeft.SetActive(true);
+    }
+
+    public void OnFireLeft()
+    {
+        waterHandLeft.SetActive(false);
+        grassHandLeft.SetActive(false);
+        fireHandLeft.SetActive(true); 
+    }
+
+    public void OnWaterLeft()
+    {
+        grassHandLeft.SetActive(false);
+        fireHandLeft.SetActive(false);
+        waterHandLeft.SetActive(true); 
+    }
+
+    //Right Hand
+    public void OnGrassRight()
+    {
+        fireHandRight.SetActive(false);
+        waterHandRight.SetActive(false);
+        grassHandRight.SetActive(true);
+    }
+
+    public void OnFireRight()
+    {
+        waterHandRight.SetActive(false);
+        grassHandRight.SetActive(false);
+        fireHandRight.SetActive(true);
+    }
+
+    public void OnWaterRight()
+    {
+        grassHandRight.SetActive(false);
+        fireHandRight.SetActive(false);
+        waterHandRight.SetActive(true);
+    }
+}
--- Assets/Script/HandShap.cs
+++ Assets/Script/HandShap.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class HandShap : MonoBehaviour
 {
     public List<GameObject> color;
-    public int teri;
+    public int index;
     public GameObject currentC;
 
 
     public List<GameObject> color2;
-    public int teri2;
+    public int index2;
     public GameObject currentC2;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentC = color[teri];
-        teri = 0;
+        currentC = color[index];
+        index = 0;
 
-        currentC2 = color2[teri2];
-        teri2 = 0;
+        currentC2 = color2[index2];
+        index2 = 0;
 
     }
 
@@ -35,24 +35,24 @@ public class HandShap : MonoBehaviour
         if(other.CompareTag("Hand"))
         {
             currentC.SetActive(false);
-            currentC = color[teri += 1];
+            currentC = color[index += 1];
  
 
 
             if (currentC == color[3])
             {
-               teri = 0;
+               index = 0;
 
             }
             currentC.SetActive(true);
 
 
             currentC2.SetActive(false);
-            currentC2 = color2[teri2 += 1];
+            currentC2 = color2[index2 += 1];
 
             if (currentC2 == color2[3])
             {
-                teri2 = 0;
+                index2 = 0;
             }
             currentC2.SetActive(true);
         }
